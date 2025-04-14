05d5fcaba3b19412de835a72b4559e214e6df48c
Amy Hegarty <76706603+amyhegarty@users.noreply.github.com>
2025-04-14 12:39:38 -0600

    add text file helper functions

--- _helper_functions/utils.py
+++ _helper_functions/utils.py
@@ -96,4 +96,62 @@ def searchfiles(path, dryrun=False, find_first=False):
             files = files[0]
 
         return files
-    
+
+
+def replace_line(filename, pattern, repl):
+    """
+        Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+        `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+        """
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+    # writing with updating). This is usually a good thing. In this case,
+    # however, binary writing imposes non-trivial encoding constraints trivially
+    # resolved by switching to text writing. Let's do that.
+    with tempfile.NamedTemporaryFile(dir=os.getcwd(), mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                if re.findall(pattern_compiled, line):
+                    tmp_file.write(repl)
+                else:
+                    tmp_file.write(line)
+
+    # Overwrite the original file with the munged temporary file in a
+    # manner preserving file attributes (e.g., permissions).
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, filename)
+
+
+
+def locate_by_pattern(filename, pattern):
+    """
+    Locates all instances that meet pattern and returns value from file.
+    Args:
+        filename: text file
+        pattern: regex
+
+    Returns:
+
+    """
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+    arr = []
+    with open(filename) as src_file:
+        for line in src_file:
+            num = re.findall(pattern_compiled, line)
+            if num:
+                arr.append(num[0])
+
+    return arr
+
+
+def create_file(filename):
+    try:
+        f = open(filename, "x")
+        f.close()
+        print("File created successfully.")
+    except FileExistsError:
+        print("File already exists.")
+
